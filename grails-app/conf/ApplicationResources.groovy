883e09e6b0dae13f82b0c86510188942d69d621b
Steinway Wu <steinwaywhw@gmail.com>
2013-06-13 12:42:54 -0400

    Prepare to branch

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,26 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+    bootstrap {
+    	dependsOn 'jquery'
+
+    	resource url: 'bootstrap/js/bootstrap.min.js'
+    	resource url: 'bootstrap/css/bootstrap-responsive.min.css'
+    	resource url: 'bootstrap/css/bootstrap.min.css'
+    }
+
+    ckeditor {
+    	dependsOn 'jquery'
+
+    	resource url: 'ckeditor/ckeditor.js'
+    }
+
+    tageditor {
+    	dependsOn 'jquery, bootstrap'
+
+    	resource url: 'js/bootstrap-tagmanager.js'
+    	resource url: 'css/bootstrap-tagmanager.css'
+    }
+
 }
